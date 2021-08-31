760ae2ace9a98da8869c42bd9396f23cafde08e2
ADARSH N <adarshkumar738291@gmail.com>
2021-08-31 21:36:06 +0530

    Add files via upload

---
+++ adding_bullets.py
@@ -0,0 +1,8 @@
+import pyperclip
+text = pyperclip.paste()
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+    text = '\n'.join(lines)
+pyperclip.copy(text)
