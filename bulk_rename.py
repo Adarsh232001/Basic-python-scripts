f34e7bc17f89fc0f926880f5af1fbf3c580a4137
ADARSH N <adarshkumar738291@gmail.com>
2021-09-02 07:09:51 +0530

    Add files via upload

---
+++ bulk_rename.py
@@ -0,0 +1,16 @@
+import os
+def main():
+    i=0
+    #Enter the path by replacing \ to /.
+    path = "D:/WhatsApp Images/Sent/"
+    for filename in os.listdir(path):
+        my_dest = "img" + str(i) + ".jpg"
+        my_source =path + filename
+        my_dest =path + my_dest
+        os.rename(my_source, my_dest)
+        i+= 1
+ 
+if __name__=='__main__':
+    main()
+
+        
