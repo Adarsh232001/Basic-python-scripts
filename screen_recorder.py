66c32d9c0478f63e196c29fb90ca616d339b5ec1
ADARSH N <adarshkumar738291@gmail.com>
2021-09-15 16:32:30 +0530

    Add files via upload

---
+++ screen_recorder.py
@@ -0,0 +1,17 @@
+from PIL import ImageGrab
+import numpy as np 
+import cv2
+from win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+forcecc = cv2.VideoWriter_fourcc('m','p','4','v')
+captured_video = cv2.VideoWriter('output.mp4', forcecc, 20.0, (width, height))
+while True:
+    img = ImageGrab.grab(bbox=(0, 0 ,width, height))
+    img_np = np.array(img)
+    img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Secrete Capture', img_final)
+    captured_video.write(img_final)
+    if cv2.waitKey(10) == ord('q'):
+        break
